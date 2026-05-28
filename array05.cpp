d04dede5e46c1bd9ed904b5408fc690b31727a52
AyushPal <palayush8858@gmail.com>
2026-05-28 22:57:52 +0530

    second largest element in an array

---
+++ array05.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[100];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int largest=arr[0];
+    int slargest=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            slargest=largest;
+            largest=arr[i];
+        }
+        else if(arr[i]>slargest && arr[i]<largest){
+            slargest=arr[i];
+        }
+    }
+    cout<<slargest;
+    return 0;
+}
