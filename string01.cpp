154f6cabebef191649331b840d51fa24f2595976
AyushPal <palayush8858@gmail.com>
2026-06-09 23:47:43 +0530

    count conosonent

---
+++ string01.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string s;
+    getline(cin,s);
+
+    int vowels = 0;
+    int letters = 0;
+
+    for(char ch : s){
+
+        ch = tolower(ch);
+
+        if(ch>='a' && ch<='z'){
+            letters++;
+
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                vowels++;
+            }
+        }
+    }
+
+    cout << letters - vowels;
+
+    return 0;
+}
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s);
+    int cnt=0;
+    for(char ch:s){
+        ch=tolower(ch);
+        if(ch>='a' && ch<='z' && !(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
