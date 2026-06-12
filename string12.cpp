f6aee3a94d0be68b8332b17d1a73a824b80aed83
AyushPal <palayush8858@gmail.com>
2026-06-12 23:28:32 +0530

    string rotation

---
+++ string12.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    string goal;
+    cin>>goal;
+    if(s.size()!=goal.size()){
+        cout<<"false";
+        return 0;
+    }
+    string temp=s+s;
+    cout<<(temp.find(goal)!=string::npos?"true":"false");
+   
+    return 0;
+
+}
+
