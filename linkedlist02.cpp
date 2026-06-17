9ba86c020eb464ba88bd8d757b5fc3a8d87e5435
AyushPal <palayush8858@gmail.com>
2026-06-18 03:12:16 +0530

    deletion in ll by head,end,position,value

--- linkedlist02.cpp
+++ linkedlist02.cpp
@@ -1,64 +1,176 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- class node{
-    public:
+class node
+{
+public:
     int data;
-    node* next;
-    public:
-    node(int data1,node*next1){
-        data=data1;
-        next=next1;
+    node *next;
+
+public:
+    node(int data1, node *next1)
+    {
+        data = data1;
+        next = next1;
     }
 
-    //multiple costrictor
-    public:
-    node(int data1){
-        data=data1;
-        next=nullptr;
+    // multiple costrictor
+public:
+    node(int data1)
+    {
+        data = data1;
+        next = nullptr;
     }
- };
+};
 
- node* converarr2ll(vector <int> &arr){
-    node* head= new node(arr[0]);
-    node* mover =head;
-    for(int i=0;i<arr.size();i++){
-        node* temp = new node(arr[i]);
-        mover->next=temp;
-        mover=mover->next;
+node *converarr2ll(vector<int> &arr)
+{
+    node *head = new node(arr[0]);
+    node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        node *temp = new node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
     }
     return head;
- }
-
- //length of ll
-int lengthOfLL(Node* head) {
-int cnt = 0;
-Node* temp = head;
-while(temp) {
-temp = temp->next;
-cnt++;
-
-return cnt;
 }
 
+// length of ll
+int lengthOfLL(node *head)
+{
+    int cnt = 0;
+    node *temp = head;
+    while (temp)
+    {
+        temp = temp->next;
+        cnt++;
+    }
 
-//search in ll
-int checkIfPresent(Node* head, int val) {
-Node temp = head;
-while(temp) {
-if(temp->data == val) return 1;
-temp = temp->next;
+    return cnt;
 }
-return 0;
+
+// print ll
+void print(node *head)
+{
+
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
 }
 
- int main(){
-    vector<int> arr= {2,5,8,7};
-   node* head =converarr2ll(arr);
-    // node* temp= head;
-    // while(temp){
-    //     cout<<temp->data<<" ";
-    //     temp=temp->next;
-    // }
+// search in ll
+int checkIfPresent(node *head, int val)
+{
+    node *temp = head;
+    while (temp)
+    {
+        if (temp->data == val)
+            return 1;
+        temp = temp->next;
+    }
+    return 0;
+}
 
-    // cout<<lengthOfLL(head);
- }
+// delete the head
+node *removehead(node *head)
+{
+    if (head == NULL)
+        return head;
+    node *temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+// delete the last node
+node *removetail(node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return NULL;
+    }
+    node *temp = head;
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+    free(temp->next);
+    temp->next = nullptr;
+
+    return head;
+}
+
+// remove specific position node
+node *removeposition(node *head, int k)
+{
+    if (head == NULL)
+        return head;
+    if (k == 1)
+    {
+        node *temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    int cnt = 0;
+    node *temp = head;
+    node *prev = NULL;
+    while (head != NULL)
+    {
+        cnt++;
+        if (cnt == k)
+        {
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+// remove node by value
+node *removevalue(node *head, int el)
+{
+    if (head == NULL)
+        return head;
+    if (head->data == el)
+    {
+        node *temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+
+    node *temp = head;
+    node *prev = NULL;
+    while (head != NULL)
+    {
+        if (temp->data == el)
+        {
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+int main()
+{
+    vector<int> arr = {2, 5, 8, 7};
+    node *head = converarr2ll(arr);
+    //  head=removehead(head);
+    //  head=removetail(head);
+
+    //    head=removeposition(head,3);
+    head = removevalue(head, 7);
+    print(head);
+
+    return 0;
+}
