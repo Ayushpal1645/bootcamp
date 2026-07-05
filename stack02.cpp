363bac2c0adee53e4f53dfee86bfb8cc21968d98
AyushPal <palayush8858@gmail.com>
2026-07-06 02:28:05 +0530

    valid parenthesis

--- stack02.cpp
+++ stack02.cpp
@@ -1,16 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    string s;
-    cin>>s;
-    stack <char> st;
-    for(char ch:s){
-        st.push(ch);
+bool isvalid(string s)
+{
+    stack<char> st;
+    for (char ch : s)
+    {
+        if (ch == '[' || ch == '{' || ch == '(')
+        {
+            st.push(ch);
+        }
+
+        else
+        {
+            if (st.empty())
+            {
+                cout << "not valid";
+                return false;
+            }
+            else
+            {
+                if (ch=='}'&&st.top()=='{'||
+                    ch==']'&&st.top()=='['||
+                    ch==')'&&st.top()=='('){
+                        st.pop();
+                    }
+            }
+        }
     }
-    for(char ch:s){
-        cout<<st.top();
-        st.pop();
+    return true;
+}
+int main()
+{
+    string s;
+    cin >> s;
+    if (isvalid(s))
+    {
+        cout << "Valid";
+    }
+    else
+    {
+        cout << "not valid";
     }
     return 0;
-
 }
